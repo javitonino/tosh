e096ee435264849ab644c6b27d643352446d2b2a
Javier Torres <jtorres@carto.com>
2017-09-24 20:56:41 +0200

    Better config sample

--- tosh/config.py
+++ tosh/config.py
@@ -10,18 +10,6 @@ class Config:
         self._config = yaml.load(fh)
         fh.close()
 
-        self._clouds = {}
-        for name, cloud in self.get('servers', 'clouds').items():
-            cloud['name'] = name
-            for p in cloud['public']:
-                self._clouds[p] = cloud
-
-    def get_clouds(self):
-        return self.get('servers', 'clouds').values()
-
-    def get_cloud(self, public):
-        return self._clouds[public]
-
     def get(self, *args):
         """Get a key from a path given as multiple strings from the config file."""
         value = self._config
